e27f61c4b72b48e6f12e1a8dcfa02d5f698a738d
aeldenburg <aeldenburg@gmail.com>
2019-08-13 16:51:48 -0700

    Game fully working, need to add message for winner and more themeing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <div className="header">
+      <div className="title">
+          <h1>Welcome to the React Clicky Game: NBA Edition</h1>
+          <br></br>
+          <h3>HOW TO PLAY</h3>
+          <h6>Click images of NBA players in order to earn points. Don't click the same card twice!</h6>
+          <br></br>
+      </div>
+    <div className="scores">
+      <h4>Score: {props.score}</h4>
+    </div>
+    <div className="highscore">
+     <h6>Highscore: {props.highscore}</h6>
+    </div>
+  </div>
+);
+
+export default Header;
