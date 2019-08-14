1e73bb62f3e250facea251a9aec838c530cd581f
aeldenburg <aeldenburg@gmail.com>
2019-08-14 00:12:34 -0700

    updated theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,31 @@ import "./Header.css";
 var Header = props => (
   <div className="header">
       <div className="title">
-          <h1>Welcome to the React Clicky Game: NBA Edition</h1>
+      <div className="about-card">
+        <div className="card-body">
+          <h1>React Clicky Game: NBA Edition</h1>
           <br></br>
           <h3>HOW TO PLAY</h3>
           <h6>Click images of NBA players in order to earn points. Don't click the same card twice!</h6>
-          <br></br>
+        </div>
       </div>
+      <br></br>
+      </div>
+
     <div className="scores">
-      <h4>Score: {props.score}</h4>
+      <div class="score-card">
+        <div class="card-body">
+          <h4>Score: {props.score}</h4>
+        </div>
+      </div>
     </div>
+
     <div className="highscore">
-     <h6>Highscore: {props.highscore}</h6>
+    <div class="high-card">
+      <div class="card-body">
+      <h6>Highscore: {props.highscore}</h6>
+      </div>
+    </div>
     </div>
   </div>
 );
